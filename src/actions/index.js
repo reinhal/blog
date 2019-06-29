ae54923226d710bc682c3c673219cc68df8910ee
Lisa Reinhardt <reinhal@gmail.com>
2019-06-29 10:47:29 -0500

    updated action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,7 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
-export const fetchPosts = () => {
-  return async function (dispatch, getState) {
-    const response =  await jsonPlaceholder.get('/posts');
-  
-    dispatch({ type: 'FETCH_POSTS', payload: response})
-  } 
-};
+export const fetchPosts = () => async dispatch => {
+  const response =  await jsonPlaceholder.get('/posts');
+
+  dispatch({ type: 'FETCH_POSTS', payload: response})
+}; 
