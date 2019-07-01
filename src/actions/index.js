b4101323848b57e6749310bc1c27f5cac99c39e6
Lisa Reinhardt <reinhal@gmail.com>
2019-06-30 22:09:46 -0500

    added chaining

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,9 +4,11 @@ import jsonPlaceholder from '../apis/jsonPlaceholder';
 export const fetchPostsAndUsers = () => async (dispatch, getState) => {
   await dispatch(fetchPosts());
 
-  const userIds = _.uniq(_.map(getState().posts, 'userId'));
-
-  userIds.forEach(id => dispatch(fetchUser(id)));
+  _.chain(getState().posts)
+    .map('userId')
+    .uniq()
+    .forEach(id => dispatch(fetchUser(id)))
+    .value();
 };
 
 export const fetchPosts = () => async dispatch => {
@@ -20,11 +22,3 @@ export const fetchUser = id => async dispatch => {
 
   dispatch({ type: 'FETCH_USER', payload: response.data });
 };
-
-// export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
-
-// const _fetchUser = _.memoize(async (id, dispatch) => {
-//   const response = await jsonPlaceholder.get(`/users/${id}`);
-
-//   dispatch ({ type: 'FETCH_USER', payload: response.data });
-// }); 
